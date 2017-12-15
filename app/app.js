cc0169b6398436029b4fd9176006534958ed56fe
mcdonndi <mcdonndi@tcd.ie>
2017-12-15 15:03:34 +0000

    Workers add numbers from an array

--- app/app.js
+++ app/app.js
@@ -2,24 +2,51 @@ const cluster = require('cluster');
 const http = require('http');
 const numCPUs = require('os').cpus().length;
 
+let nums = [1,2,3,4,5,6,7,8,9,10,11,12];
+let workers = [];
+
 if (cluster.isMaster) {
     console.log(`Master ${process.pid} is running`);
+    let allTotal = 0;
 
     // Fork workers.
     for (let i = 0; i < numCPUs; i++) {
-        cluster.fork();
+        const worker = cluster.fork();
+        workers.push(worker);
+
+        //Listen for returned work
+        worker.on('message', function(message) {
+            console.log(`Master ${process.pid} recevies message '${JSON.stringify(message)}' from worker ${worker.process.pid}`);
+            allTotal += message.total;
+            console.log(allTotal);
+        });
     }
+
+
+    //Send work to workers
+    workers.forEach((worker) => {
+        console.log(`Master ${process.pid} sends work to worker ${worker.process.pid}...`);
+        let someNums = nums.splice(0,4);
+        worker.send({ nums: someNums });
+    }, this);
 
     cluster.on('exit', (worker, code, signal) => {
         console.log(`worker ${worker.process.pid} died`);
-});
+    });
 } else {
-    // Workers can share any TCP connection
-    // In this case it is an HTTP server
-    http.createServer((req, res) => {
-        res.writeHead(200);
-    res.end('hello world\n');
-}).listen(8000);
-
     console.log(`Worker ${process.pid} started`);
+    let total = 0;
+
+    process.on('message', function(message) {
+        console.log(`Worker ${process.pid} recevies message '${JSON.stringify(message)}'`);
+        message.nums.forEach((value) =>{
+            total += value;
+        });
+
+        console.log(`Worker ${process.pid} sends message to master...`);
+        process.send({ total: total});
+    });
+
+    console.log(`Worker ${process.pid} finished`);
 }
+
